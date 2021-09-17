1965b7d897851b4b9fd303f926e87c2c96caaa78
Joel Dias Filho <joeldiasfilho@hotmail.com>
2021-09-17 18:36:46 -0300

    commit inicial

---
+++ src/test/java/com/joel/explorando_marte/ExplorandoMarteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joel.explorando_marte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExplorandoMarteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
